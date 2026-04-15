e33e09ccc1be2a92d9a782c23086eb56f80a81da
Emma Ebeling <ebelinge@msu.edu>
2026-04-15 17:47:16 +0000

    changed to team supabase

--- supabase-config.js
+++ supabase-config.js
@@ -2,8 +2,8 @@
     "use strict";
 
     global.WAYS_TO_SAVE_CONFIG = {
-        supabaseUrl: "https://zgghcmbxvwbinmolifmf.supabase.co",
-        supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpnZ2hjbWJ4dndiaW5tb2xpZm1mIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzUzNDMxMDEsImV4cCI6MjA5MDkxOTEwMX0._l8hGiwde5x4aiIty0z8uc6fOkgCV2C1YoIscj7Ry40",
+        supabaseUrl: "https://xwiaojpbahlwyzzozjvq.supabase.co",
+        supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh3aWFvanBiYWhsd3l6em96anZxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU3MjQzODEsImV4cCI6MjA5MTMwMDM4MX0.G3B5ZCKcTCYpEBVUoQhWxNEiqhJrQqTe0_DdKnhmuTA",
         supabaseTable: "saved_draws",
     };
 })(typeof window !== "undefined" ? window : globalThis);
